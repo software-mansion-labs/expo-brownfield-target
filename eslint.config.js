e01ccfb0dc328322a6f905ca2c9fdc2108558e67
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-23 14:04:54 +0200

    chore: set up eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierPluginRecommendedConfig from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default [
+  {
+    ignores: [
+      'app.plugin.js',
+      'build/',
+      'example/',
+      'node_modules/',
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierPluginRecommendedConfig,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+];
