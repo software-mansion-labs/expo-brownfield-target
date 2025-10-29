50d73223a9bf80ed18668015ad85eb133b6ea343
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-29 19:22:38 +0100

    fix: fix patch-expo.sh script for ios

--- plugin/ios/templates/patch-expo.sh
+++ plugin/ios/templates/patch-expo.sh
@@ -1,8 +1,11 @@
 FILE="${SRCROOT}/Pods/Target Support Files/Pods-${{projectName}}-${{targetName}}/ExpoModulesProvider.swift"
+TEMP_FILE="$FILE.temp"
 
 if [ -f "$FILE" ]; then
   echo "Patching $FILE to hide Expo from public interface"
-  sed -i 's/^import EX/internal import EX/' "$FILE"
-  sed -i 's/^import Ex/internal import Ex/' "$FILE"
-  sed -i 's/public class ExpoModulesProvider/internal class ExpoModulesProvider/' "$FILE"
+  sed \\
+    -e 's/^import EX/internal import EX/' \\
+    -e 's/^import Ex/internal import Ex/' \\
+    -e 's/public class ExpoModulesProvider/internal class ExpoModulesProvider/' "$FILE" > "$TEMP_FILE"
+  mv "$TEMP_FILE" "$FILE"
 fi
