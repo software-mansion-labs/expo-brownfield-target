2741816e785a830e07b7c5a5d7f6397463b1aeac
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-30 10:14:58 +0100

    build: initialize xcode project at example/ios

---
+++ example/ios/ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by Patryk Mleczek on 10/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
