477f514c44703ee6ff7220fbe40dee63be66237b
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-30 12:57:41 +0100

    feat: add brownfield app to example/ios

--- example/ios/ios/ViewController.swift
+++ example/ios/ios/ViewController.swift
@@ -5,23 +5,30 @@
 //  Created by Patryk Mleczek on 10/30/25.
 //
 
+import MyBrownfieldApp
 import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let label = UILabel()
-        label.text = "Hello from native app 👋"
-        label.textColor = .white
+        let reactNativeView = MyBrownfieldApp
+            .ReactNativeHostManager
+            .shared
+            .loadView(
+                moduleName: "main",
+                initialProps: nil,
+                launchOptions: [:]
+            )
         
-        view.backgroundColor = .black
-        view.addSubview(label)
+        view.addSubview(reactNativeView)
         
-        label.translatesAutoresizingMaskIntoConstraints = false
+        reactNativeView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            reactNativeView.topAnchor.constraint(equalTo: view.topAnchor),
+            reactNativeView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            reactNativeView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            reactNativeView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
 }
