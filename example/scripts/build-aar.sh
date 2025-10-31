7d18bac7876da8a87b80501ea0765e7847885337
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-31 09:51:57 +0100

    chore: update build-aar.sh

--- example/scripts/build-aar.sh
+++ example/scripts/build-aar.sh
@@ -3,5 +3,10 @@ set -e
 
 cd app/android
 
+echo "Building brownfield library..."
+
+echo "Building debug AAR..."
 ./gradlew :brownfield:assembleDebug
+
+echo "Building release AAR..."
 ./gradlew :brownfield:assembleRelease
