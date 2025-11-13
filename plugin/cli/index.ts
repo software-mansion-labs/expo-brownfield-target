5fea8bdcabde22e686399e0376ea3c7994aa5bc9
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-13 14:27:43 +0100

    feat: add basic cli

---
+++ plugin/cli/index.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import { GENERAL_HELP_MESSAGE, UNKNOWN_COMMAND_MESSAGE } from './messages';
+// @ts-expect-error
+import { version } from '../../../package.json';
+import { handleBuild } from './handling';
+import type { CLIAction } from './types';
+
+const parseArgs = (args: string[]): CLIAction => {
+  if (args.length < 1 || args[0] === '-h' || args[0] === '--help') {
+    return 'help';
+  }
+
+  if (args[0] === '-v' || args[0] === '--version') {
+    return 'version';
+  }
+
+  if (args[0] === 'build') {
+    return 'build';
+  }
+
+  return 'unknown';
+};
+
+const main = async () => {
+  const args = process.argv.slice(2);
+  const action = parseArgs(args);
+  switch (action) {
+    case 'build':
+      handleBuild(args.slice(1));
+      process.exit(0);
+    case 'help':
+      console.log(GENERAL_HELP_MESSAGE);
+      process.exit(0);
+    case 'version':
+      console.log(version);
+      process.exit(0);
+    case 'unknown':
+      console.log(UNKNOWN_COMMAND_MESSAGE(args[0]));
+      process.exit(1);
+    default:
+      process.exit(0);
+  }
+};
+
+main();
