70ecf0a67fac8dcab3c01a4bfbf3ec5950e40c33
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-17 13:44:20 +0100

    chore: unify templating system

--- plugin/ios/utils/filesystem.ts
+++ plugin/ios/utils/filesystem.ts
@@ -1,5 +1,8 @@
-import fs, { writeFileSync } from 'node:fs';
-import path from 'node:path';
+import fs from 'node:fs';
+import {
+  createFileFromTemplate as createFileFromTemplateCommon,
+  createFileFromTemplateAs as createFileFromTemplateAsCommon,
+} from '../../common/filesystem';
 
 export const mkdir = (path: string, recursive: boolean = false) => {
   fs.mkdirSync(path, {
@@ -7,53 +10,12 @@ export const mkdir = (path: string, recursive: boolean = false) => {
   });
 };
 
-const interpolateVariables = (
-  str: string,
-  variables: Record<string, unknown>,
-): string => {
-  const variableRegex = /\${{[A-z0-9]+}}/;
-  let match = variableRegex.exec(str);
-  while (match) {
-    const variable = match[0].slice(3, -2);
-    str = str.replace(match[0], String(variables[variable]));
-    match = variableRegex.exec(str);
-  }
-
-  return str;
-};
-
-const readTemplate = (template: string): string => {
-  const templatesPath = path.join(__filename, '../..', 'templates');
-  const templatePath = path.join(templatesPath, template);
-
-  if (!fs.existsSync(templatePath)) {
-    throw new Error();
-  }
-
-  return fs.readFileSync(templatePath).toString();
-};
-
-const createFileFromTemplateInternal = (
-  template: string,
-  at: string,
-  dest: string,
-  variables?: Record<string, unknown>,
-) => {
-  let templateContents = readTemplate(template);
-  if (variables) {
-    templateContents = interpolateVariables(templateContents, variables);
-  }
-
-  const destPath = path.join(at, dest);
-  writeFileSync(destPath, templateContents);
-};
-
 export const createFileFromTemplate = (
   template: string,
   at: string,
   variables?: Record<string, unknown>,
 ) => {
-  createFileFromTemplateInternal(template, at, template, variables);
+  createFileFromTemplateCommon(template, at, 'ios', variables);
 };
 
 export const createFileFromTemplateAs = (
@@ -62,17 +24,5 @@ export const createFileFromTemplateAs = (
   as: string,
   variables?: Record<string, unknown>,
 ) => {
-  createFileFromTemplateInternal(template, at, as, variables);
-};
-
-export const readFromTemplate = (
-  template: string,
-  variables?: Record<string, unknown>,
-): string => {
-  let templateContents = readTemplate(template);
-  if (variables) {
-    templateContents = interpolateVariables(templateContents, variables);
-  }
-
-  return templateContents;
+  createFileFromTemplateAsCommon(template, at, as, 'ios', variables);
 };
