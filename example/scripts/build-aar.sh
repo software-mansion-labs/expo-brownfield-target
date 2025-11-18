2d37ca11f9f8cb70b10a992867c1b8187ff14ac7
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-18 16:24:45 +0100

    chore: update build-aar.sh to support dynamic library names

--- example/scripts/build-aar.sh
+++ example/scripts/build-aar.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 set -e
 
+LIBRARY_NAME=${LIBRARY_NAME:-"brownfield"}
+
 cd app/android
+
+if [ -d "$LIBRARY_NAME" ]; then
+  echo "LIBRARY_NAME is not defined"
+  echo "Searching for brownfield library name..."
+
+  LIBRARY_NAME=$(basename $(dirname $(find . -name 'ReactNativeFragment.kt')))
+  if [ -z "$LIBRARY_NAME" ]; then
+    echo "Error: couldn't infer the brownfield library name"
+    exit 1
+  fi
+
+  echo -e "Inferred the library name to be: $LIBRARY_NAME\n"
+fi
 
 echo "Building brownfield library..."
 
 echo "Running ./gradlew clean..."
-./gradlew :brownfield:clean
+./gradlew :$LIBRARY_NAME:clean
 
 # echo "Building debug AAR..."
 # ./gradlew :brownfield:assembleDebug
 
 echo "Building release AAR..."
-./gradlew :brownfield:assembleRelease
+./gradlew :$LIBRARY_NAME:assembleRelease
 
 echo "Publishing AAR to local Maven repo..."
-./gradlew :brownfield:publishMavenAarPublicationToMavenLocal
+./gradlew :$LIBRARY_NAME:publishMavenAarPublicationToMavenLocal
