392d98186caa38b0be5153be60ae396ffbb212fb
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-19 10:46:35 +0100

    fix: fix build-aar.sh library name inference

--- example/scripts/build-aar.sh
+++ example/scripts/build-aar.sh
@@ -5,13 +5,19 @@ LIBRARY_NAME=${LIBRARY_NAME:-"brownfield"}
 
 cd app/android
 
-if [ -d "$LIBRARY_NAME" ]; then
+if [ ! -d "$LIBRARY_NAME" ]; then
   echo "LIBRARY_NAME is not defined"
   echo "Searching for brownfield library name..."
 
-  LIBRARY_NAME=$(basename $(dirname $(find . -name 'ReactNativeFragment.kt')))
-  if [ -z "$LIBRARY_NAME" ]; then
+  FOUND_PATH=$(find . -name 'ReactNativeFragment.kt' | head -1)
+  if [ -z "$FOUND_PATH" ]; then
     echo "Error: couldn't infer the brownfield library name"
+    exit 1
+  fi
+
+  LIBRARY_NAME=$(echo "$FOUND_PATH" | sed 's|^\./||' | cut -d/ -f1)
+  if [ -z "$LIBRARY_NAME" ]; then
+    echo "Error: couldn't extract brownfield library name from path: $FOUND_PATH"
     exit 1
   fi
 
@@ -24,7 +30,7 @@ echo "Running ./gradlew clean..."
 ./gradlew :$LIBRARY_NAME:clean
 
 # echo "Building debug AAR..."
-# ./gradlew :brownfield:assembleDebug
+# ./gradlew :$LIBRARY_NAME:assembleDebug
 
 echo "Building release AAR..."
 ./gradlew :$LIBRARY_NAME:assembleRelease
