c46f72dfe430c3be72b7ef579d1b0bb3b871b4d6
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-22 13:46:07 +0100

    chore: remove explicit host manager initialization from ios example apps

--- example/ios/ios/AppDelegate.swift
+++ example/ios/ios/AppDelegate.swift
@@ -11,7 +11,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        MyBrownfieldApp.ReactNativeHostManager.shared.initialize()
         return true
     }
 
