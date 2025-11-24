6b9b30d5c655cd32b364e7df5efdaec73a0c7d85
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-24 10:26:48 +0100

    refactor: switch to programatic react native fragment creation

--- example/android/app/src/main/java/com/pmleczek/android/MainActivity.kt
+++ example/android/app/src/main/java/com/pmleczek/android/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 import com.pmleczek.expobrownfieldtargetexample.brownfield.ReactNativeHostManager
+import com.pmleczek.expobrownfieldtargetexample.brownfield.ReactNativeFragment
 
 class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ReactNativeHostManager.shared.initialize(this.application)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        val rnFragment = ReactNativeFragment.createFragmentHost(
+            activity = this,
+        )
+        setContentView(rnFragment)
     }
 
     override fun invokeDefaultOnBackPressed() {
