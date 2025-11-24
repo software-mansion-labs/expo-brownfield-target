60dbf7be3086679e37cbfeb4c5200faf9dd7b446
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-24 14:08:57 +0100

    feat: improve ios react native loader reliability

--- plugin/templates/ios/ReactNativeLoader.m
+++ plugin/templates/ios/ReactNativeLoader.m
@@ -9,12 +9,25 @@
 @implementation ReactNativeHostManagerLoader
 
 + (void)load {
-    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification
-                                                      object:nil
-                                                       queue:[NSOperationQueue mainQueue]
-                                                  usingBlock:^(NSNotification * _Nonnull note) {
-        [[ReactNativeHostManager shared] initialize];
-    }];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        UIApplication *application = [UIApplication sharedApplication];
+        if (application && application.applicationState != UIApplicationStateBackground) {
+            [[ReactNativeHostManager shared] initialize];
+        } else {
+            __block id<NSObject> observer = nil;
+            
+            observer = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification
+                                                                          object:nil
+                                                                           queue:[NSOperationQueue mainQueue]
+                                                                      usingBlock:^(NSNotification * _Nonnull note) {
+                [[ReactNativeHostManager shared] initialize];
+                if (observer) {
+                    [[NSNotificationCenter defaultCenter] removeObserver:observer];
+                }
+            }];
+        }
+    });
 }
 
 @end
