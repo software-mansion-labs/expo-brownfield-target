4c6d654cd666d60508412453163dcff9831c8096
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-24 10:49:46 +0100

    feat: simplify brownfield initialization on android

--- example/android/app/src/main/java/com/pmleczek/android/MainActivity.kt
+++ example/android/app/src/main/java/com/pmleczek/android/MainActivity.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
-import com.pmleczek.expobrownfieldtargetexample.brownfield.ReactNativeHostManager
-import com.pmleczek.expobrownfieldtargetexample.brownfield.ReactNativeFragment
+import com.pmleczek.expobrownfieldtargetexample.brownfield.showReactNativeFragment
 
 class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ReactNativeHostManager.shared.initialize(this.application)
         enableEdgeToEdge()
-
-        val rnFragment = ReactNativeFragment.createFragmentHost(
-            activity = this,
-        )
-        setContentView(rnFragment)
+        showReactNativeFragment()
     }
 
     override fun invokeDefaultOnBackPressed() {
