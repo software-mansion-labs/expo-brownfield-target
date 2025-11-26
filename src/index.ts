5780eaa4fd46845ca1f2e9ca65967c77026ce415
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-26 15:51:23 +0100

    feat: implement popToNative for android

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-// Reexport the native module. On web, it will be resolved to ExpoBrownfieldModule.web.ts
-// and on native platforms to ExpoBrownfieldModule.ts
-// export { default } from './ExpoBrownfieldModule';
-// export { default as ExpoBrownfieldModuleView } from './ExpoBrownfieldModuleView';
-// export * from './ExpoBrownfieldModule.types';
 import ExpoBrownfieldModule from './ExpoBrownfieldModule';
 
-export const hello = () => {
-  return ExpoBrownfieldModule?.hello() ?? '';
+export const popToNative = (): void => {
+  return ExpoBrownfieldModule?.popToNative();
 };
+
+// export const setNativeBackEnabled = (enabled: boolean): void => {
+//   return ExpoBrownfieldModule?.setNativeBackEnabled(enabled);
+// };
