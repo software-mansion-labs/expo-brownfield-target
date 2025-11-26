8ec379f9fffff151c3adcbeaf35dab30b2fc974c
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-26 15:01:20 +0100

    chore: display react native fragment in a separate activity

--- example/android/app/src/main/java/com/pmleczek/android/MainActivity.kt
+++ example/android/app/src/main/java/com/pmleczek/android/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.pmleczek.android
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
-import com.pmleczek.expobrownfieldtargetexample.brownfield.showReactNativeFragment
 
-class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        showReactNativeFragment()
-    }
-
-    override fun invokeDefaultOnBackPressed() {
-        TODO("Not yet implemented")
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.openReactNativeButton).setOnClickListener {
+            startActivity(Intent(this, ReactNativeActivity::class.java))
+        }
     }
 }
