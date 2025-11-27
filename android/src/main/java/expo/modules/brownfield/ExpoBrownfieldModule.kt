fc0951ff2a0add3bb0ecf2a6525e270987c68e70
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-11-27 15:15:18 +0100

    chore: cleanup

--- android/src/main/java/expo/modules/brownfield/ExpoBrownfieldModule.kt
+++ android/src/main/java/expo/modules/brownfield/ExpoBrownfieldModule.kt
@@ -4,15 +4,10 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoBrownfieldModule : Module() {
-  companion object {
-    var shouldPopToNative: Boolean = false
-  }
-
   override fun definition() = ModuleDefinition {
     Name("ExpoBrownfieldModule")
 
     Function("popToNative") { animated: Boolean ->
-      shouldPopToNative = true
       appContext.currentActivity?.runOnUiThread {
         @Suppress("DEPRECATION")
         appContext.currentActivity?.onBackPressed()
