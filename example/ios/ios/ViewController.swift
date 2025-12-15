f8471073bc264ddc99f49b3f2398c8875f578a11
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-12-15 12:41:52 +0100

    chore: add communication examples to uikit app

--- example/ios/ios/ViewController.swift
+++ example/ios/ios/ViewController.swift
@@ -1,7 +1,19 @@
 import MyBrownfieldApp
 import UIKit
 
+let dateFormatter: DateFormatter = {
+  let formatter = DateFormatter()
+  formatter.locale = Locale(identifier: "en_US_POSIX")
+  formatter.dateFormat = "dd MMM yyyy HH:mm:ss"
+  return formatter
+}()
+
+// MARK: - ViewController
+
 class ViewController: UIViewController {
+  private var listenerId: String?
+  private var timer: Timer?
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -18,11 +30,61 @@ class ViewController: UIViewController {
       button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
       button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
     ])
+
+    listenerId = BrownfieldMessaging.addListener { message in
+      print("Message from React Native received:")
+      print(message)
+    }
+
+    startSendingMessages()
+  }
+
+  deinit {
+    stopSendingMessages()
+    if let id = listenerId {
+      BrownfieldMessaging.removeListener(id: id)
+    }
   }
 
   @objc
   private func openReactNativeApp() {
     let reactNativeViewController = ReactNativeViewController(moduleName: "main")
     navigationController?.pushViewController(reactNativeViewController, animated: true)
+  }
+
+  private func startSendingMessages() {
+    timer = Timer.scheduledTimer(
+      withTimeInterval: 5.0,
+      repeats: true
+    ) { [weak self] _ in
+      self?.sendMessage()
+    }
+  }
+
+  private func stopSendingMessages() {
+    timer?.invalidate()
+    timer = nil
+  }
+
+  private func sendMessage() {
+    let message: [String: Any] = [
+      "sender": "iOS UIKit app",
+      "receiver": "React Native app",
+      "data": [
+        "array": [1, 2, 3.5, true, "hello", false, ["key": "value"]],
+        "object": [
+          "nested": ["key": "value"],
+        ],
+        "number": 123.456,
+        "boolean": false,
+        "string": "hello",
+      ],
+      "metadata": [
+        "timestamp": dateFormatter.string(from: Date()),
+        "platform": "iOS (UIKit)",
+      ],
+    ]
+
+    BrownfieldMessaging.sendMessage(message)
   }
 }
