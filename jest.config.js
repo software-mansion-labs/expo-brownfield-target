b5889be55d08426f341e0283424488d34c7edca0
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-12-16 13:32:02 +0100

    test: add tests for cli/utils/config.ts

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+  collectCoverage: true,
   verbose: true,
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          isolatedModules: true,
+          noImplicitAny: false,
+        },
+      },
+    ],
   },
 };
