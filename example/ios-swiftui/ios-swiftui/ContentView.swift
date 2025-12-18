b863ff0c5ee564ca180062233601008f37825ba6
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-12-18 15:06:12 +0100

    chore: update native apps

--- example/ios-swiftui/ios-swiftui/ContentView.swift
+++ example/ios-swiftui/ios-swiftui/ContentView.swift
@@ -51,8 +51,18 @@ struct ContentView: View {
   }
 
   private func startSendingMessages() {
-    timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
-      sendMessage()
+    // Run on background thread so that interaction like scrolling
+    // don't block the messaging
+    DispatchQueue.global(qos: .background).async {
+      self.timer = Timer.scheduledTimer(
+        withTimeInterval: 5.0,
+        repeats: true
+      ) { [self] _ in
+        self.sendMessage()
+      }
+      let runLoop = RunLoop.current
+      runLoop.add(self.timer as! Timer, forMode: .default)
+      runLoop.run()
     }
   }
 
